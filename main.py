993b02c170b19c62c89417afdd8ae4e171af7383
229916 Kolibabski Maciej <229916@edu.p.lodz.pl>
2022-03-05 21:28:47 +0100

    Zapis do excela gotowy

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import pandas as pd
 # 'C:/Users/macie/Desktop/DiffPgCo.xlsx.xlsx'
 
 def excel_dim(path):
-    global outputx, outputy
+    global outputx, outputy, wyjscie2
     df = pd.read_excel(path)
-    df_new = df[['Name']]
     df_s = df["Name"].str.lower()
-    tabliczka=[]
+    tabx = []
+    taby = []
     for m in range(df_s.size):
         small_sentence = df_s[m]
         tab_location = []
@@ -47,15 +47,31 @@ def excel_dim(path):
         pos_end = tab_spaces_with_x[2]
 
         if (small_sentence[pos_x - 1].isdigit() and small_sentence[pos_x + 1].isdigit()):
-            wyjscie=''
+            wyjscie = ''
             for i in range(pos_start, pos_end):
                 outputx = small_sentence[i].replace("\n", "")
                 wyjscie += outputx
-            wyjscie2=wyjscie.replace('x',' ').split()
-            print(wyjscie2[1], end="")
+            wyjscie2 = wyjscie.replace('x', ' ').split()
+            print(wyjscie2[0], end="" + ' ' + wyjscie2[1])
+            tabx.append(wyjscie2[0])
+            taby.append(wyjscie2[1])
 
-    return outputx
+    try:
+        a1 = pd.read_excel(path, header=0)
+    except:
+        print('ERROR')
+
+    else:
+        nextrow = a1.shape[0]
+        for elem in range(0, nextrow):
+            a1.loc[elem, 'X'] = tabx[elem]
+            a1.loc[elem, 'Y'] = taby[elem]
+        a1.to_excel(path, index=False, header=True)
+    finally:
+        print('Writing to excel file succesfully executed')
+
+    return True
 
 
 if __name__ == '__main__':
-    (excel_dim('C:/Users/macie/Desktop/DiffPgCo.xlsx'))
+    excel_dim('C:/Users/macie/Desktop/DiffPgCo.xlsx')
